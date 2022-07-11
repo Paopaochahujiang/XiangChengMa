4a18fda41b1ed54bb2b6af1109bacff09bcba8c2
ishuaige <1394293788@qq.com>
2022-07-11 19:28:45 +0800

    项目初始化

---
+++ app/src/main/java/com/baiyun/xiangchengma/MainActivity.java
@@ -0,0 +1,14 @@
+package com.baiyun.xiangchengma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
